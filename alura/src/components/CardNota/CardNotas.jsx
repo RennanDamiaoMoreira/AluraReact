56e43a239510085ce7ac9e359384637058b0f8aa
Rennan <rennandamiao@gmail.com>
2021-04-05 19:29:35 -0300

    Update CardNotas.jsx

--- alura/src/components/CardNota/CardNotas.jsx
+++ alura/src/components/CardNota/CardNotas.jsx
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
-import "../../css/materialize.css"
+import "../../css/materialize.css";
 class CardNotas extends Component {
   render() {
     return (
-      <div className="card blue-grey darken-1">
-        <div className="card-content white-text">
-          <span className="card-title">Card Title</span>
-          <p>I am a very simple card. I am good at containing small bits of information.
-          I am convenient because I require little markup to use effectively.</p>
+      <div>
+        <div className="card blue-grey darken-1 ">
+          <div className="card-content white-text">
+            <span className="card-title">Card Title</span>
+            <p>
+              I am a very simple card. I am good at containing small bits of
+              information. I am convenient because I require little markup to
+              use effectively.
+            </p>
+          </div>
         </div>
-        </div>
+      </div>
     );
   }
 }
