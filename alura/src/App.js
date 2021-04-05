1da01984d74cfedbff00364467fe49dbbad49c8d
Rennan <rennandamiao@gmail.com>
2021-04-05 15:37:06 -0300

    Update App.js

--- alura/src/App.js
+++ alura/src/App.js
@@ -1,8 +1,11 @@
-
-
+import React from 'react';
 function App() {
   return (
-   
+  <form>
+    <input type="text" placeholder="título"/>
+    <textarea placeholder="insira sua nota"/>
+    <button type ="submit">enviar</button>
+  </form>
   );
 }
 
