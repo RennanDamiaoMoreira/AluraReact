69850735962464b995c68b337b5979866637deb3
Rennan <rennandamiao@gmail.com>
2021-04-05 19:29:30 -0300

    Update App.js

--- alura/src/App.js
+++ alura/src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from "react";
 import ListaDeNotas from "./components/ListaDeNotas";
 import FormularioCadastro from "./components/fomularioCadastro";
-import "./css/materialize.css"
+import "./css/materialize.css";
 class App extends Component {
   render() {
     return (
       <section className="container">
-        <FormularioCadastro></FormularioCadastro>
-        <ListaDeNotas></ListaDeNotas>
+        <div className="row">
+          <div className="col s12 l4"><FormularioCadastro></FormularioCadastro></div>
+          <div className="col s12 l8"><ListaDeNotas></ListaDeNotas></div>
+        </div>
+        
+        
+        
       </section>
     );
   }
