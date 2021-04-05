0cdcf975ea6a4cd687da0c67bb9c76a1d930a410
Rennan <rennandamiao@gmail.com>
2021-04-05 17:14:40 -0300

    Create CardNotas.jsx

---
+++ alura/src/components/CardNota/CardNotas.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "../../css/materialize.css"
+class CardNotas extends Component {
+  render() {
+    return (
+      <div className="card blue-grey darken-1">
+        <div className="card-content white-text">
+          <span className="card-title">Card Title</span>
+          <p>I am a very simple card. I am good at containing small bits of information.
+          I am convenient because I require little markup to use effectively.</p>
+        </div>
+        </div>
+    );
+  }
+}
+
+export default CardNotas;
