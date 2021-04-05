fcda27a5b88ff7686fdb0c5662f8579f6efb0fe4
Rennan <rennandamiao@gmail.com>
2021-04-05 15:53:03 -0300

    Update fomularioCadastro.jsx

--- alura/src/components/fomularioCadastro.jsx
+++ alura/src/components/fomularioCadastro.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
- export class formularioCadastro extends Component{
-     render(){
-         return(<form>
-            <input type="text" placeholder="título" />
-            <textarea placeholder="insira sua nota" />
-            <button type="submit">enviar</button>
-          </form>);
-     }
- }
+export class formularioCadastro extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="título" />
+        <textarea placeholder="insira sua nota" />
+        <button type="submit">enviar</button>
+      </form>
+    );
+  }
+}
