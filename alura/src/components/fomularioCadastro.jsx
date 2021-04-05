44d398403ba08cff4539dd3160bd987dae2cbad3
Rennan <rennandamiao@gmail.com>
2021-04-05 19:29:38 -0300

    Update fomularioCadastro.jsx

--- alura/src/components/fomularioCadastro.jsx
+++ alura/src/components/fomularioCadastro.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import "../css/materialize.css"
 class FormularioCadastro extends Component {
+  constructor (){
+    super();
+    this.titulo="";
+    this.nota="";
+  }
+  handleMudancaTitulo(evento){
+    this.titulo = evento.target.value;
+  }
   render() {
     return (
       <form>
-        <input type="text" placeholder="titulo"></input>
+        <input type="text" placeholder="titulo"  onChange={this.handleMudancaTitulo}></input>
         <textarea name="" placeholder="insira sua nota aqui"></textarea>
-        <button type="submit">enviar</button>
+        <button className="btn " type="submit" >enviar</button>
       </form>
     );
   }
