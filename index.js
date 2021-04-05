d58ec0c4b25ce4785c8c8e26ee0ab59b6adc67bc
Rennan <rennandamiao@gmail.com>
2021-04-05 11:38:41 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import{Cliente} from "./Cliente.js";
 const cliente1 = new Cliente();
 cliente1.nome="rennan";
 cliente1.cpf= "11122233300";
-const conta = new ContaCorrente();
+const conta = new ContaCorrente("123","100",cliente1);
 conta.deposita(10);
 console.log(cliente1,conta)
 conta.sacar(4);
