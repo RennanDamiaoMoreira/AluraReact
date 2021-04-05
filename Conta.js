e7aa6ba1d1cd1ad7839bbe890984db9090095771
Rennan <rennandamiao@gmail.com>
2021-04-05 10:26:42 -0300

    Update Conta.js

--- Conta.js
+++ Conta.js
@@ -1,4 +1,11 @@
 class ContaCorrente{
     agencia;
     saldo;
+    sacar(valor){
+        if (valor<saldo){
+            saldo=saldo-valor;
+        }else{
+            console.log("erro ao sacar saldo inferior ao pedido, gostaria de fazer um empréstmo?");
+        }
+    }
 }
