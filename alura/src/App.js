c159abfaf814832294521b5e62f4db4c865675f9
Rennan <rennandamiao@gmail.com>
2021-04-05 17:14:37 -0300

    Update App.js

--- alura/src/App.js
+++ alura/src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
-import { formularioCadastro } from "./components/fomularioCadastro";
-import ListaDeNotas from "./components/ListaDeNotas"
-
-function App() {
-  return (
-    <section>
-      <formularioCadastro/>
-      <ListaDeNotas/>
-    </section>
-  );
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/fomularioCadastro";
+import "./css/materialize.css"
+class App extends Component {
+  render() {
+    return (
+      <section className="container">
+        <FormularioCadastro></FormularioCadastro>
+        <ListaDeNotas></ListaDeNotas>
+      </section>
+    );
+  }
 }
 
 export default App;
