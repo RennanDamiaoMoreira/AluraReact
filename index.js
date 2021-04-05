08d0eb6854b7c1c0fe236f468317489b0b00688b
Rennan <rennandamiao@gmail.com>
2021-04-05 10:17:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const cliente1Cpf="11122233309";
 const cliente1Agencia="1001";
 const cliente1Saldo=0;
  console.log(cliente1Nome);
+
+ 
+ const cliente=new Cliente();
+ cliente.nome="rennan";
+  console.log(cliente.nome);
