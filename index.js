503c69d2ab00beb3e02d520133c731acfeba20a1
Rennan <rennandamiao@gmail.com>
2021-04-05 11:26:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-
-const cliente1Nome="Ricardo";
-const cliente1Cpf="11122233309";
-const cliente1Agencia="1001";
-const cliente1_saldo=0;
- console.log(cliente1Nome);
-
- 
- const cliente=new Cliente();
- cliente.nome="rennan";
-  console.log(cliente.nome);
+import {ContaCorrente} from "./Conta.js";
+import{Cliente} from "./Cliente.js";
+const cliente1 = new Cliente();
+cliente1.nome="rennan";
+cliente1.cpf= "11122233300";
+const conta = new ContaCorrente();
+conta.deposita(10);
+console.log(cliente1,conta)
