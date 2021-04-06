720e4c78a5799237a3d891bc10ef4cf16b311d73
Rennan <rennandamiao@gmail.com>
2021-04-05 21:48:49 -0300

    Update App.js

--- alura/src/App.js
+++ alura/src/App.js
@@ -6,16 +6,21 @@ class App extends Component {
   constructor(){
     super();
     this.notas=[];
+    
   }
   criarNota(titulo,texto){
    const novaNota = {titulo,texto};
    this.notas.push(novaNota);
+   this.setState({
+     
+   })
+   alert(this.notas.length)
   }
   render() {
     return (
       <section className="container">
         <div className="row">
-          <div className="col s12 l4"><FormularioCadastro criarNota={this.criarNota}></FormularioCadastro></div>
+          <div className="col s12 l4"><FormularioCadastro criarNota={this.criarNota.bind(this)}></FormularioCadastro></div>
           <div className="col s12 l8"><ListaDeNotas notas={this.notas}></ListaDeNotas></div>
         </div>
         
