a2271e01652035e37c4308d61ecfe2ec63e7245b
Rennan <rennandamiao@gmail.com>
2021-04-06 11:33:28 -0300

    Update App.js

--- alura/src/App.js
+++ alura/src/App.js
@@ -21,6 +21,11 @@ class App extends Component {
   render() {
     return (
       <section className="container">
+        <div className="center">
+          <div className="header blue-text lighten-1">
+            <h3>Sistema de cadastro de lembretes</h3>
+          </div>
+        </div>
         <div className="row">
           <div className="col s12 l4"><FormularioCadastro criarNota={this.criarNota.bind(this)}></FormularioCadastro></div>
           <div className="col s12 l8"><ListaDeNotas notas={this.state.notas}></ListaDeNotas></div>
