4fa7db1b90c61e655ebeffca337b580f3fc5a0d0
Rennan <rennandamiao@gmail.com>
2021-04-05 21:38:19 -0300

    Update ListaDeNotas.jsx

--- alura/src/components/ListaDeNotas.jsx
+++ alura/src/components/ListaDeNotas.jsx
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import CardNotas from "./CardNota/CardNotas";
 
 class ListaDeNotas extends Component {
+  
   render() {
     return (
       <ul className="">
-        {Array.of("Trabalho", "estudo", "vagabs","tops").map((categoria,index) => {
+        {this.props.notas.map((nota,index) => {
           return (
             <li key={index}  className="col s12 l4">
-              <CardNotas />
+              <CardNotas  nota={this.props.notas[index]}/>
             </li>
           );
         })}
