f317cad44dbfb1dddee42987222663d38f8e7374
Rennan <rennandamiao@gmail.com>
2021-04-06 11:33:32 -0300

    Update CardNotas.jsx

--- alura/src/components/CardNota/CardNotas.jsx
+++ alura/src/components/CardNota/CardNotas.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from "react";
 import "../../css/materialize.css";
 class CardNotas extends Component {
+  constructor(){
+    super();
+    this.handler = this.handler.bind(this);
+  }
+  excluirNota(evento){
+    evento.preventDefault();
+    evento.stopPropagation()
+    this.props.excluirNota("this.props.bind(this.props.index)");
+
+  }
   render() {
     return (
       <div>
@@ -9,7 +19,14 @@ class CardNotas extends Component {
             <span className="card-title">{this.props.titulo}</span>
             <p>
               {this.props.texto}
+              <br/>
+              {this.props.index}
+
             </p>
+            <div class="card-action">
+          
+          <button className="btn red" onClick={this.excluirNota.bind(this)}>Excluir</button>
+        </div>
           </div>
         </div>
       </div>
