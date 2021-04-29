919ffe9e88a1e442fb0c1d37f66aad12af7a64f3
Rennan <rennandamiao@gmail.com>
2021-04-29 18:05:38 -0300

    Update ListaDeCategorias.jsx

--- alura/src/components/ListaDeCategorias/ListaDeCategorias.jsx
+++ alura/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 class ListaDeCategorias extends Component {
+    eventInput(e){
+        if (e.key=="Enter"){
+            
+        }
+    }
   state = {};
   render() {
     return (
       <section>
         <ul>
-          <li>categorias</li>
-          <li>categorias</li>
-          <li>categorias</li>
-          <li>categorias</li>
+          <li>
+              <button className="btn">categorias</button>
+              </li>
+          
         </ul>
-        <input type="text" name="" id="" />
+        <input type="text" name="" id="" placeholder="Add Categoria " onKeyUp={this.eventInput.bind(this)} />
       </section>
     );
   }
