36f8a24bd984882883fce49cb35c5d6cb6b8947c
Rennan <rennandamiao@gmail.com>
2021-04-29 17:49:55 -0300

    Create ListaDeCategorias.jsx

---
+++ alura/src/components/ListaDeCategorias/ListaDeCategorias.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+class ListaDeCategorias extends Component {
+  state = {};
+  render() {
+    return (
+      <section>
+        <ul>
+          <li>categorias</li>
+          <li>categorias</li>
+          <li>categorias</li>
+          <li>categorias</li>
+        </ul>
+        <input type="text" name="" id="" />
+      </section>
+    );
+  }
+}
+
+export default ListaDeCategorias;
